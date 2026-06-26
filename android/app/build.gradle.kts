f1eed8b02c71ffcaa18a8549a50f4d84c128674f
Yamini S <yamini@Yaminis-MacBook-Pro.local>
2026-06-26 19:11:52 +0530

    fix: Androif build spec change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
     // Kotlin plugin is already included by flutter-gradle-plugin
 }
 
